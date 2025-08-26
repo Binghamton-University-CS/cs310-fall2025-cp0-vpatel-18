c3c5d7d2b8e654cd3e24ab1e674e8a9e7ae81dd4
vpatel18 <vpatel18@cs.binghamton.edu>
2025-08-26 13:15:49 -0400

    day1troubleshoot2

---
+++ AnimalsInZoo.cpp
@@ -0,0 +1,18 @@
+#include "AnimalsInZoo.h"
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std; 
+
+	AnimalsInZoo::Animals(unsigned int animalNum, Animal species){
+		numAnimals = 1;
+		animal = species
+	}; 
+
+	AnimalInZoo::AnimalsInZoo() : numAnimals, animal(){};
+
+	void AnimalInZoo::display(){
+	
+
+	
+	}
---
+++ AnimalsInZoo.h
@@ -0,0 +1,15 @@
+class AnimalsInZoo{
+
+	public: 
+		AnimalsInZoo(unsigned int numAnimals, Animal animal);
+	
+		AnimalsInZoo() : numAnimals(1), animal(){};
+
+		void display();
+
+	private: 
+		unsigned int numAnimals = 1 
+		Animal animal();
+
+
+}
